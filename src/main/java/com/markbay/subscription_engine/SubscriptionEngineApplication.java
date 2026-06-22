f27ece8a780c035e4b5a987dbaaaefe445b3c4d3
Bayonle010 <afeezonadiran@gmail.com>
2026-06-22 16:19:48 +0100

    update: enables scheduling

--- src/main/java/com/markbay/subscription_engine/SubscriptionEngineApplication.java
+++ src/main/java/com/markbay/subscription_engine/SubscriptionEngineApplication.java
@@ -2,7 +2,9 @@ package com.markbay.subscription_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SubscriptionEngineApplication {
 
