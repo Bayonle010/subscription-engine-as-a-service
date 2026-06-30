ded1b5a641e57ecfa67c84f4d562bc5594e4d9d3
Bayonle010 <afeezonadiran@gmail.com>
2026-06-30 18:54:24 +0100

    fix: changes nomba webhook url

--- src/main/java/com/markbay/subscription_engine/webhook/controller/NombaWebhookController.java
+++ src/main/java/com/markbay/subscription_engine/webhook/controller/NombaWebhookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/webhooks/nomba")
+@RequestMapping("/webhooks/nomba")
 @RequiredArgsConstructor
 public class NombaWebhookController {
 
